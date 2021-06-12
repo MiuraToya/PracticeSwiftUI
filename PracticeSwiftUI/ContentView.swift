53d2fa9ded39d9d6307843c4269c9a8b45121e1c
Miura Toya <dengzais8@gmail.com>
2021-06-12 15:17:22 +0900

    add

--- PracticeSwiftUI/ContentView.swift
+++ PracticeSwiftUI/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+ var body: some View {
+  HStack(spacing: -15.0) {
+   Image("GitHubMark")
+    .resizable()
+    .frame(width: 44.0, height: 44.0)
+   VStack(alignment: .leading, spacing: -30.0) {
+    Text("Owner Name")
+     .font(.caption)
+     .fontWeight(.regular)
+     .foregroundColor(Color.black)
+     .padding()
+    Text("Repositoly Name")
+     .font(.body)
+     .fontWeight(.semibold)
+     .foregroundColor(Color.black)
+     .padding()
+   }
+  }
+ }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+ static var previews: some View {
+  ContentView()
+ }
 }
