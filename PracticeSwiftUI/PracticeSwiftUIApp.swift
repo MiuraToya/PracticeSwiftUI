4f3a6b1fc624de34cc26553809a326834d22300f
Miura Toya <dengzais8@gmail.com>
2021-06-12 14:33:27 +0900

    Initial Commit

---
+++ PracticeSwiftUI/PracticeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeSwiftUIApp.swift
+//  PracticeSwiftUI
+//
+//  Created by 三浦　登哉 on 2021/06/12.
+//
+
+import SwiftUI
+
+@main
+struct PracticeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
